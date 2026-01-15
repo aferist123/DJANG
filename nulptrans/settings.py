ee918022005269a87b2cc7b41065b0da119085bf
aferist123 <gnidetsoleh@gmail.com>
2026-01-15 22:03:58 +0200

    added audio player and exam banner

--- nulptrans/settings.py
+++ nulptrans/settings.py
@@ -127,3 +127,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 LOGIN_URL = 'two_factor:login'
 LOGIN_REDIRECT_URL = '/'
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Цей рядок змушує Render "побачити" твій audio.mp3
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
